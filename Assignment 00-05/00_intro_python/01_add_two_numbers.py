ddd905285be2429f406441636c3672bf04cc9ea3
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-07 04:41:12 +0500

    expression 1

---
+++ Assignment 00-05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,9 @@
+print("This app will sum up the integars")
+
+num1 = int(input("Enter the first Number : "))
+
+num2 = int(input("Enter the Second Number : "))
+
+total_sum = num1 + num2
+
+print(f"The total sum of {num1} and {num2} is {total_sum}.")
