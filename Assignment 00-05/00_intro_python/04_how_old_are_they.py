36eecd2bfc1c503821f17bfd9fa513339376e946
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-07 05:09:34 +0500

    add 04 in 00

---
+++ Assignment 00-05/00_intro_python/04_how_old_are_they.py
@@ -0,0 +1,13 @@
+
+Anton : int = 21
+Beth : int = (Anton + 6)
+Chen: int = (Beth + 20)
+Drew: int =(Anton + Chen)
+Ethan : int = (Chen)
+
+#Print all of their ages:
+print(f"Anton's age is {Anton}.")
+print(f"Beth's age is {Beth}.")
+print(f"Chen's age is {Chen}.")
+print(f"Drew's age is {Drew}.")
+print(f"Ethan's age is {Ethan}.")
