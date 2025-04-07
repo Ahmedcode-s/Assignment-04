697ecce8d3896878d23e15286ceaeaf7c80115d6
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-07 21:31:39 +0500

    added feet to inches

---
+++ Assignment 00-05/01_expressions/03_feet_to_inches.py
@@ -0,0 +1,12 @@
+
+inches_in_foot: int = 12
+
+def main():
+    
+    user_feet = float(input("Enter Number of feet: "))
+    print(f"That is {user_feet * inches_in_foot} inches!")
+
+
+
+if __name__ == '__main__':
+    main()
