7812240007009e768479d0e9ec4920e83c8b6ec5
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-07 05:19:51 +0500

    05 triangle perim

---
+++ Assignment 00-05/00_intro_python/05_triangle_perimeter.py
@@ -0,0 +1,8 @@
+#Asking user to Enter the length of the sides of the triangle:
+
+s1 = float(input("Enter the length of side 1 of the triangle :"))
+s2 = float(input("Enter the length of side 2 of the triangle :"))
+s3 = float(input("Enter the length of side 3 of the triangle :"))
+
+#Final Result Message:
+print("The Perimeter of your triangle is " + str(s1 + s2 + s3))
