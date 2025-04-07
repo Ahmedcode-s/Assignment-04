ffdca21bcf29c6901cb90829322801c07b533425
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-07 05:25:57 +0500

    added 06 in 00

---
+++ Assignment 00-05/00_intro_python/06_square_number.py
@@ -0,0 +1,4 @@
+#Ask the user to enter a number to check its square : 
+user_input = float(input("Enter the number to check it's square : "))
+
+print(f"The square of {user_input} is " + str(user_input ** 2) )
