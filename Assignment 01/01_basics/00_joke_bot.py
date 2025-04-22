929c1a7944842a2292db764a82e4590cb894188c
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-22 20:49:13 +0500

    Changed joke bot

--- Assignment 01/01_basics/00_joke_bot.py
+++ Assignment 01/01_basics/00_joke_bot.py
@@ -1,15 +1,18 @@
-PROMPT: str = "What do you want? "
+PROMPT: str = "What do you want?"
 JOKE: str = "Here is a joke for you! Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs'"
-SORRY: str = "Sorry I only tell jokes."
+SORRY: str = "Sorry I only tell jokes. Type joke to get a laugh."
 
 def main():
+    print(PROMPT)
     user_input = input()
     user_input = user_input.strip().lower()
     
-    if "joke" in user_input:
-        print(JOKE)
-    else:
+    while True:
+        if "joke" in user_input:
+            print(JOKE)
+            break
         print(SORRY)
-
+        user_input= input()
+    
 if __name__ == "__main__":
     main()
